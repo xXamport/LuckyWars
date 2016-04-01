0c2509fe3995f21527b7f6dc954c393094756d68
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-04-01 20:50:05 +0300

    Update LuckyWars.php

--- src/Nawaf_Craft1b/LuckyWars.php
+++ src/Nawaf_Craft1b/LuckyWars.php
@@ -1,1 +1,26 @@
+<?php
 
+namespace Nawaf_Craft1b;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\level\Level;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+use MiniGamesTree\PlayerSettings\PlayerAPI as GamesAPI;
+
+class LuckyWars extends PluginBase implements Listener{
+  
+  public function getGamesAPI(){
+  return GamesAPI::getInstance();
+  }
+  
+  public function onEnable(){
+  #register
+  
+  $this->getServer()->getPluginManager()->registerEvents($this,$this);
+  
+  }
+  
+}
